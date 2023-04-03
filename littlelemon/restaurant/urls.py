cbd6e1521cd7fa342f502e8a6e422b5283bdf6a9
Ali Hamidi <a.hamidida@gmail.com>
2023-04-04 03:13:31 +0430

    added APIs

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index,name='index'),
+    path('', views.MenuItemView.as_view()),
+    path('<int:pk>/', views.SingleMenuItemView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView 
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .serializers import MenuSerializer,BookingSerializer
+from .models import Menu,Booking
 
 def index(request):
     return render(request,'index.html',{})
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
